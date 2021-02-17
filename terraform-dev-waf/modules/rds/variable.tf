af900322e18ef6ad6bee395d391767cf503ccce9
Alin002 <gilcaalin@gmail.com>
2021-02-17 17:32:18 +0200

    exclude rds

--- terraform-dev-waf/modules/rds/variable.tf
+++ terraform-dev-waf/modules/rds/variable.tf
@@ -1,12 +1,12 @@
-variable "db_instance_type" {
-    description = "The instance type of the RDS instance"
-    type        = string
-    default     = "db.m6g.large"
-}
+# variable "db_instance_type" {
+#     description = "The instance type of the RDS instance"
+#     type        = string
+#     default     = "db.m6g.large"
+# }
 
-#variable "aws_db_subnet_group"{}
-#variable "rds_private_subnet"{}
-variable "rds_sg"{}
+# #variable "aws_db_subnet_group"{}
+# #variable "rds_private_subnet"{}
+# variable "rds_sg"{}
 
 
 
